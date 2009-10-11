b49cdba826626857c5bb904c4f5c488990c2ebf2
pamela.fox <pamela.fox@99de6746-39ed-11de-b310-9106b1f7c239>
2009-10-11 12:48:43 +0000

    Adding exporty sample
    git-svn-id: http://google-wave-resources.googlecode.com/svn/trunk/samples@72 99de6746-39ed-11de-b310-9106b1f7c239

--- extensions/robots/python/exporty/models.py
+++ extensions/robots/python/exporty/models.py
@@ -1,14 +1,3 @@
-#!/usr/bin/python2.4
-#
-# Copyright 2009 Google Inc. All Rights Reserved.
-
-"""One-line documentation for models module.
-
-A detailed description of models.
-"""
-
-__author__ = 'pamelafox@google.com (Pamela Fox)'
-
 from google.appengine.ext import db
 
 class WaveExport(db.Model):
